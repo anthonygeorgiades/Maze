23bb5e76c029cf9db20f60e60375f0918774e18e
Anthony Georgiades <anthonygeorgiades@gmail.com>
2017-04-13 23:35:07 -0400

    First commit

---
+++ DisjointSet.java
@@ -0,0 +1,37 @@
+package maze;
+
+public class DisjointSet {
+    
+    //TODO - write all the methods of this class
+    
+    /**
+     * make a set out of the cells in the maze
+     * @param maze
+     */
+    public void makeSet(MazeCell[][] maze) {
+        
+    }
+
+    /**
+     * Create the union of the sets that contain cell1 and cell2.
+     * If the two cells are in the same set, nothing changes,
+     * else create the new set as a union. 
+     * Please see the union-find algorithm.
+     * @param cell1
+     * @param cell2
+     */
+    public void union(MazeCell cell1, MazeCell cell2){
+        
+    }
+
+    /**
+     * Find the set that the cell is a part of.
+     * While finding the set, do the path compression as well.
+     * @param cell
+     * @return
+     */
+    public MazeCell find(MazeCell cell){
+        return null;
+    }
+
+}
